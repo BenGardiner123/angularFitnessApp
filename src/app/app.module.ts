293e5d3528c2b6afb5f1e35c2ab6e04e3ab3021e
Ben Gardiner <bennygards@gmail.com>
2021-08-04 12:41:43 +1000

    adding the flex-layout packsge creating a log in page testing the on submit works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
@@ -28,7 +32,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule, 
+    FormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
